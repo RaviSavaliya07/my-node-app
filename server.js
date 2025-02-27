c0f562d00a4ff667a9f12fbb8fb06c3d69f0a0b0
RaviSavaliya07 <ravisavaliyar171@gmail.com>
2025-02-27 03:56:20 +0000

    Fix: Listen on 0.0.0.0 to allow external access

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ app.get("/", (req, res) => {
 require("./app/routes/tutorial.routes.js")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
